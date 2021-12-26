c9e287bbfc207b9f94c605151f6cb2682cdf04dc
Matija Kocbek <mkocbek1@gmail.com>
2021-12-26 16:38:47 +0100

    Branje prilagojeno operacijskemu sistemu Windows

--- domaca-naloga/main.ml
+++ domaca-naloga/main.ml
@@ -1,5 +1,5 @@
 let read_problem filename =
-  let channel = open_in filename in
+  let channel = open_in_bin filename in
   let str = really_input_string channel (in_channel_length channel) in
   close_in channel;
   Model.problem_of_string str
